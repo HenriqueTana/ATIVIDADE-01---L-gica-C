0b608514b033c1970b8aee68d665e71de7d5e421
Henrique Mustafa Tanajura Junior <henriquejr.08@hotmail.com>
2024-05-10 20:55:38 -0300

    Add files via upload

---
+++ Algoritmo 2 num.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//Faça um algoritmo que receba dois números e exiba o resultado da sua soma. 
+main(){
+	
+	int numero_1, numero_2, total;
+	printf("\n Escreva o numero 1: ",numero_1);
+	scanf("%d",&numero_1);
+	printf("\n Escreva o numero 2: ",numero_2);
+	scanf("%d",&numero_2);
+
+	total = numero_1 + numero_2; 
+	printf(" O Total e : %d ", total );	
+}
+
